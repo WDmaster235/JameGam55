7a5d5c1d20354ef6332e01d9cb9c08262951e92e
raz-habarvaz <tommy.shafransky3@gmail.com>
2026-05-30 16:23:55 +0300

    EnemySpanerScript added
    EnemySpanerScript added

---
+++ Assets/Prefabs/Enemies/EnemySpawner/EnemySpawnerScript.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class EnemySpawnerScript : MonoBehaviour
+{
+
+
+    int budget;
+    EnemyScript[] enemies;
+
+
+    // Creates the queue that decides the order in which we push the enemies each round
+    Queue<int> enemyQueue()
+    {
+
+
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
